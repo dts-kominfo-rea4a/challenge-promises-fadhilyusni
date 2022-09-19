0ae3d47387f8b19d4d275c84932b22c90786da90
fadhilyusni <42725126+fadhilyusni@users.noreply.github.com>
2022-09-19 21:10:25 +0700

    push promise

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async (emotion) => {
+  let data1 = await promiseTheaterIXX();
+  let data = data1.concat(await promiseTheaterVGC());
+
+  let count = 0;
+  data.forEach((element) => {
+    if (element.hasil == emotion) {
+      count++;
+    }
+  });
+
+  return count;
+};
 
 module.exports = {
   promiseOutput,
